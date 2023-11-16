7f902555eb7479745605af57a611a9b19c5eb431
Prashant Singh <37827327+prashantlakha@users.noreply.github.com>
2023-11-16 11:54:15 -0500

    updates

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
+import Logo from "./dojo-logo.png";
+
 function Navbar() {
   return (
     <nav>
+      <Image src={Logo} alt='Dojo Helpdesk Logo' width={70} quality={100} />
       <h1>Dojo helpdesk </h1>
       <Link href='/'>Dashboard</Link>
       <Link href='/tickets'>Tickets</Link>
